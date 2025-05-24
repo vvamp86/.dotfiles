5bfcc30326e42ae97f7cd528f1da305ae2e2220a
vvamp86 <ryao.7970@gmail.com>
2025-05-24 05:10:38 -0400

    functionals with new stuff

--- functionals.sh
+++ functionals.sh
@@ -1,3 +1,16 @@
 yay -S discord_arch_electron obsidian cloudflare-warp-bin proton-vpn-gtk-app brave-bin spotify-adblock timeshift quickemu
 
 sudo pacman -S fwupd blueman rate-mirrors keepassxc qemu-full easyeffects calf lsp-plugins-lv2 zam-plugins-lv2 mda.lv2
+
+# enable bluetooth
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
+
+# enable cloudflare-warp
+sudo systemctl start warp-svc
+sudo systemctl enable warp-svc
+warp-cli register
+warp-cli connect
+
+# enable proton-vpn
+
