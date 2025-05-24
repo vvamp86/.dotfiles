043c2c9c9b35cb93b9fbc66038e668c50df0e9e4
vvamp86 <ryao.7970@gmail.com>
2025-05-24 06:07:49 -0400

    misc

--- functionals.sh
+++ functionals.sh
@@ -1,6 +1,6 @@
 yay -S discord_arch_electron obsidian cloudflare-warp-bin proton-vpn-gtk-app brave-bin spotify-adblock timeshift quickemu
 
-sudo pacman -S fwupd blueman rate-mirrors keepassxc qemu-full easyeffects calf lsp-plugins-lv2 zam-plugins-lv2 mda.lv2
+sudo pacman -S fwupd blueman rate-mirrors keepassxc qemu-full easyeffects calf lsp-plugins-lv2 zam-plugins-lv2 mda.lv2 brightnessctl
 
 # enable bluetooth
 sudo systemctl start bluetooth
