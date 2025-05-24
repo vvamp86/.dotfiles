315f3635bb2a8339944f2b10ff73370b23191da6
vvamp86 <ryao7970@gmail.com>
2025-05-24 13:03:48 -0400

    added nvim clipboard and gtk settings for proper icons

---
+++ .profile
@@ -0,0 +1,4 @@
+export TERMINAL=alacritty
+export GTK_THEME="Gruvbox-Material-Dark"
+export XCURSOR_THEME="Papirus-Dark"
+#export QT_QPA_PLATFORMTHEME=qt5ct
--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo -e "${GREEN}==> Installing dependencies...${NC}"
-sudo pacman -Syu --noconfirm alacritty i3-wm neovim picom starship git ttf-firacode-nerd
+sudo pacman -Syu --noconfirm alacritty i3-wm neovim picom starship git ttf-firacode-nerd xclip
+
+yay -S gruvbox-material-theme-git
 
 echo -e "${GREEN}==> Removing existing config directories...${NC}"
-rm -rf ~/.config/alacritty ~/.config/i3 ~/.config/nvim ~/.config/picom
+rm -rf ~/.config/alacritty ~/.config/i3 ~/.config/nvim ~/.config/picom ~/.config/gtk-3.0 ~/.config/gtk-4.0 ~/.bashrc ~/.profile
 
 echo -e "${GREEN}==> Symlinking dotfiles...${NC}"
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -21,6 +23,9 @@ ln -sfn ~/.dotfiles/nvim ~/.config/nvim
 ln -sfn ~/.dotfiles/alacritty/ ~/.config/alacritty
 ln -sfn ~/.dotfiles/picom ~/.config/picom
 ln -sfn ~/.dotfiles/starship.toml ~/.config/starship.toml
+ln -sfn ~/.dotfiles/gtk-3.0 ~/.config/gtk-3.0
+ln -sfn ~/.dotfiles/gtk-4.0 ~/.config/gtk-4.0
 ln -sfn ~/.dotfiles/.bashrc ~/.bashrc
+ln -sfn ~/.dotfiles/.profile ~/.profile
 
-echo -e "${GREEN}Setup complete.${NC}"
+echo -e "${GREEN}Setup complete.${NCa}"
