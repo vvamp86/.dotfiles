609694c33b015330c3d70734edbc136cb867872f
vvamp86 <ryao.7970@gmail.com>
2025-06-27 12:44:09 -0400

    attempt to get vimtex concealer to work. :(

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -95,7 +95,10 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 -- Set conceallevel to 2 for obsidian.nvim
-vim.opt.conceallevel = 1
+vim.opt.conceallevel = 2
+
+-- set conceal for normal mode
+vim.opt.concealcursor = "nc"
 
 -- vimtex requirement
 vim.g.vimtex_view_method = "zathura"
@@ -103,4 +106,21 @@ vim.g.vimtex_view_method = "zathura"
 -- vimtex sanity patch, remove for showing warnings
 vim.g.vimtex_quickfix_ignore_filters = {
   [[Warning:]],
+}
+
+-- vimtex concealing config
+vim.g.vimtex_syntax_enabled = 1
+vim.g.vimtex_syntax_conceal = {
+  accents = 1,
+  ligatures = 1,
+  cites = 1,
+  fancy = 1,
+  greek = 1,
+  math_bounds = 1,
+  math_delimiters = 1,
+  math_fracs = 1,
+  math_super_sub = 1,
+  math_symbols = 1,
+  sections = 1,
+  styles = 1,
 }
