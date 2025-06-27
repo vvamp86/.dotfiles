621b046b2437d2466e75d43bdb8541a6f4cfbec5
vvamp86 <ryao.7970@gmail.com>
2025-06-27 12:38:25 -0400

    vimtex refactoring for simplicity

--- nvim/lua/plugins/latex/vimtex.lua
+++ nvim/lua/plugins/latex/vimtex.lua
@@ -6,6 +6,21 @@ local function is_in_obsidian_vault()
   return buf_path:find(vault_path) ~= nil
 end
 
+vim.g.vimtex_syntax_conceal = {
+  accents = 1,
+  ligatures = 1,
+  cites = 1,
+  fancy = 1,
+  greek = 1,
+  math_bounds = 1,
+  math_delimiters = 1,
+  math_fracs = 1,
+  math_super_sub = 1,
+  math_symbols = 1,
+  sections = 1,
+  styles = 1,
+}
+
 return {
   "lervag/vimtex",
   ft = { "tex", "markdown" },
@@ -16,21 +31,6 @@ return {
         vim.g.vimtex_view_method = "zathura"
         vim.g.vimtex_compiler_latexmk = {
           continuous = 1,
-        }
-
-        vim.g.vimtex_syntax_conceal = {
-          accents = 1,
-          ligatures = 1,
-          cites = 1,
-          fancy = 1,
-          greek = 1,
-          math_bounds = 1,
-          math_delimiters = 1,
-          math_fracs = 1,
-          math_super_sub = 1,
-          math_symbols = 1,
-          sections = 1,
-          styles = 1,
         }
       end,
     })
@@ -54,20 +54,6 @@ return {
           vim.g.vimtex_view_enabled = 0
           vim.g.vimtex_toc_enabled = 0
           vim.g.vimtex_complete_enabled = 0
-
-          vim.g.vimtex_syntax_conceal = {
-            accents = 1,
-            ligatures = 1,
-            cites = 1,
-            fancy = 1, greek = 1,
-            math_bounds = 1,
-            math_delimiters = 1,
-            math_fracs = 1,
-            math_super_sub = 1,
-            math_symbols = 1,
-            sections = 1,
-            styles = 1,
-          }
         end
       end,
     })
