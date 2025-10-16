b50509c66b76cf468dd316b8b791ff0b79f452d1
vvamp86 <ryao.7970@gmail.com>
2025-10-16 19:34:45 -0400

    adding a .screenlayout for quick switching

---
+++ home-dir/.screenlayout/displayport_main_side_open.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1200 --pos 2256x0 --rotate normal --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+feh --auto-reload --bg-fill /usr/share/backgrounds/wallpaper.png
---
+++ home-dir/.screenlayout/displayport_only.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP --off --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+feh --auto-reload --bg-fill /usr/share/backgrounds/wallpaper.png
